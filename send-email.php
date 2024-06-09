cf54bf494203ccdd54432b62e50914052e947748
shane-kennedy-se <216883@student.upm.edu.my>
2024-06-10 01:12:53 +0800

    Update send-email.php

--- send-email.php
+++ send-email.php
@@ -2,31 +2,31 @@
 require 'vendor/autoload.php';
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
-
-if (isset($_POST["submit"])) {
+require 'vendor/phpmailer/phpmailer/src/Exception.php';
+require 'vendor/phpmailer/phpmailer/src/PHPMailer.php';
+require 'vendor/phpmailer/phpmailer/src/SMTP.php';
+if (isset ($_POST['submit'])) {
+    $mail = new PHPMailer(true);
+    $mail ->isSMTP();
+    $mail -> Host = 'smtp.gmail.com';
+    $mail ->SMTPAuth = true;
+    $mail ->Username = 'shanejk13@gmail.com';
+    $mail ->Password = 'hkxx vmtf stbl fzxe ';
+    $mail ->SMTPSecure = 'ssl';
+    $mail ->Port = 465;
+    $mail ->setFrom('shanejk13@gmail.com');
+    $mail ->addReplyTo($_POST['email']);
+    $mail -> addAddress('shanejk13@gmail.com');
+    $mail ->isHTMl(true);
+    $mail ->Subject = 'Form Submission';
+    $mail ->Body = $_POST['message'];
     try {
-        $mail = new PHPMailer(true);
-        $mail->isSMTP();
-        $mail->Host = 'smtp.gmail.com';
-        $mail->SMTPAuth = true;
-        $mail->Username = 'shanejk13@gmail.com';
-        $mail->Password = 'ztfkhslvavrbljmr';
-        $mail->SMTPSecure = 'TLS';
-        $mail->Port = '587';
-        $mail->setFrom('shanejk13@gmail.com');
-        $mail->addAddress($_POST["email"]);
-        $mail->isHTML(true);
-        $mail->Subject = 'Form Submission';
-        $mail->Body = $_POST["message"];
-        
-        if(!$mail->send()) {
-            echo 'Message could not be sent.';
-            echo 'Mailer Error: ' . $mail->ErrorInfo;
-        } else {
-            echo 'Message has been sent';
-        }
+        $mail->send();
+        echo 'Message has been sent';
     } catch (Exception $e) {
         echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
     }
+    
+    echo "<script>alert('Message has been sent'); document.location.href='index.html'</script>";
 }
 ?>
