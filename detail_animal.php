6fdf0b2b2ba79c150b3921f690c8e7482be59e97
adibaalya <164612947+adibaalya@users.noreply.github.com>
2024-06-11 01:04:27 +0800

    detail

--- detail_animal.php
+++ detail_animal.php
@@ -24,7 +24,7 @@ if ($_GET) {
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <link rel="stylesheet" href="detail_animals.css" />
+  <link rel="stylesheet" href="detail_animal.css" />
 
   <script src="detail.js"></script>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
