217938ee38c933bea5d8a4b9bff0680e266a1f07
adibaalya <164612947+adibaalya@users.noreply.github.com>
2024-06-18 18:22:36 +0800

    done

--- php/shelter.php
+++ php/shelter.php
@@ -1,34 +1,3 @@
-
-<?php 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "pethavenuser";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
-$sql= "SELECT * from shelter";
-$result = $conn->query($sql);
-
-$shelters = [];
-if ($result->num_rows > 0) {
-    while($row = $result->fetch_assoc()) {
-        $row['imageLogo'] = base64_encode($row['imageLogo']);
-        $shelters[] = $row;
-    }
-} else {
-    echo "0 results";
-}
-$conn->close()
-?>
-
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -81,8 +50,8 @@ $conn->close()
                         <a class="nav-link" href="../html/index.html">HOME</a>
                     </li>
                     <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="../php/adopt_page.php" id="adoptDropdown" role="button"
-                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <a class="nav-link dropdown-toggle" href="../php/adopt_page.php" id="adoptDropdown"
+                            role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                             ADOPT
                         </a>
                         <div class="dropdown-menu" aria-labelledby="adoptDropdown">
@@ -127,10 +96,10 @@ $conn->close()
                         <option value="Perlis">Perlis</option>
                         <option value="Selangor">Selangor</option>
                         <option value="Kuala Lumpur">Kuala Lumpur</option>
-                          </select>
+                    </select>
 
                 </div>
-              
+
                 <div class="col-5" style="margin-left: 10px;">
                     <input type="text" class="form-control" placeholder="Shelter Name" style="border: 2px solid black;">
                 </div>
@@ -144,7 +113,7 @@ $conn->close()
     </div>
     <div class="container-fluid"
         style=" background-color:rgb(251, 249, 230); margin-top: 100px; border-radius: 50px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);  width: 80%;">
-       
+
 
         <div class="row">
             <div class="col-4">
@@ -152,116 +121,157 @@ $conn->close()
                     All
                 </div>
             </div>
-          
+
         </div>
         <div class="row d-flex justify-content-center">
-            
-           <?php foreach ($shelters as $shelter): ?>
-            <div class="col-auto">
-    <div class="container1" style="padding-left: 10px; padding-bottom: 20px;">
-        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter<?= htmlspecialchars($shelter['id']); ?>" style="background-color: rgba(0, 0, 0, 0); border: 0;">
-            <div class="card" style="border-radius: 20% 20% 10% 10%; text-align: center; width: 200px;">
+            <?php
+            $servername = "localhost";
+            $username = "root";
+            $password = "";
+            $dbname = "pethavenuser";
 
-                <div class="card-content d-flex justify-content-center align-items-center">
-                    <div class="image">
-                        <img src="data:image/jpeg;base64,<?= htmlspecialchars($shelter['imageLogo']); ?>" alt="<?= htmlspecialchars($shelter['name']); ?>" style="max-width: 100%; max-height: 100%; object-fit: contain;">
+            // Create connection
+            $conn = new mysqli($servername, $username, $password, $dbname);
+
+            // Check connection
+            if ($conn->connect_error) {
+                die("Connection failed: " . $conn->connect_error);
+            }
+
+            $sql = "SELECT * from shelter";
+            $result = $conn->query($sql);
+
+            $shelters = [];
+            if ($result->num_rows > 0) {
+                while ($row = $result->fetch_assoc()) {
+                    $row['imageLogo'] = base64_encode($row['imageLogo']);
+                    $shelters[] = $row;
+                }
+            } else {
+                echo "0 results";
+            }
+            $conn->close()
+                ?>
+
+            <?php foreach ($shelters as $shelter): ?>
+                <div class="col-auto">
+                    <div class="container1" style="padding-left: 10px; padding-bottom: 20px;">
+                        <button type="button" class="btn btn-primary" data-toggle="modal"
+                            data-target="#exampleModalCenter<?= htmlspecialchars($shelter['id']); ?>"
+                            style="background-color: rgba(0, 0, 0, 0); border: 0;">
+                            <div class="card" style="border-radius: 20% 20% 10% 10%; text-align: center; width: 200px;">
+
+                                <div class="card-content d-flex justify-content-center align-items-center">
+                                    <div class="image">
+                                        <img src="data:image/jpeg;base64,<?= htmlspecialchars($shelter['imageLogo']); ?>"
+                                            alt="<?= htmlspecialchars($shelter['name']); ?>"
+                                            style="max-width: 100%; max-height: 100%; object-fit: contain;">
+                                    </div>
+                                </div>
+
+                                <div class="container2" style="height: 70px;">
+                                    <?= htmlspecialchars($shelter['name']); ?>
+                                </div>
+                            </div>
+                        </button>
                     </div>
                 </div>
 
-                <div class="container2" style="height: 70px;">
-                    <?= htmlspecialchars($shelter['name']); ?>
-                </div>
-            </div>
-        </button>
-    </div>
-</div>
 
 
+                <div class="modal fade" id="exampleModalCenter<?= htmlspecialchars($shelter['id']); ?>" tabindex="-1"
+                    role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                    <div class="modal-dialog modal-lg" role="document">
+                        <div class="modal-content"
+                            style="background-color: #EDEBD2; border: 1px solid black; border-radius: 5%;">
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                            <div class="modal-body">
+                                <div class="container p-2" style="border-radius: 4%;">
+                                    <div class="row">
+                                        <div class="col-4 left-panel" style="background-color: white; height:fit-content">
+                                            <img src="data:image/jpeg;base64,<?= htmlspecialchars($shelter['imageLogo']); ?>"
+                                                style="width: 70%; ">
+                                            <h4><?= htmlspecialchars($shelter['name']); ?></h4>
+                                            <div class="col-auto">
+                                                <p><strong>Contact:</strong> <?= htmlspecialchars($shelter['contact']); ?>
+                                                </p>
+                                                <p><strong>Location:</strong> <?= htmlspecialchars($shelter['location']); ?>
+                                                </p>
+                                                <p><strong>Working Day:</strong>
+                                                    <?= htmlspecialchars($shelter['workingDay']); ?></p>
+                                                <p><strong>Working Hours:</strong>
+                                                    <?= htmlspecialchars($shelter['workingHours']); ?></p>
+                                            </div>
+                                        </div>
+                                        <div class="col-7 right-panel" style="margin-left: 10px; height: fit-content;">
+                                            <div class="tab-content">
+                                                <div class="tab-pane fade show active" id="description">
+                                                    <img
+                                                        src="data:image/jpeg;base64,<?= htmlspecialchars($shelter['imageDesc']); ?>">
+                                                    <p><?= htmlspecialchars($shelter['description']); ?></p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="footer-buttons text-center">
+                                        <a href="<?= htmlspecialchars($shelter['link']); ?>"><button
+                                                class="btn btn-custom">Facebook</button></a>
 
-    <div class="modal fade" id="exampleModalCenter<?= htmlspecialchars($shelter['id']); ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-        <div class="modal-dialog modal-lg" role="document">
-            <div class="modal-content" style="background-color: #EDEBD2; border: 1px solid black; border-radius: 5%;">
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-                <div class="modal-body">
-                    <div class="container p-2" style="border-radius: 4%;">
-                        <div class="row">
-                            <div class="col-4 left-panel" style="background-color: white; height:fit-content">
-                                <img src="data:image/jpeg;base64,<?= htmlspecialchars($shelter['imageLogo']); ?>" style="width: 70%; ">
-                                <h4><?= htmlspecialchars($shelter['name']); ?></h4>
-                                <div class="col-auto">
-                                    <p><strong>Contact:</strong> <?= htmlspecialchars($shelter['contact']); ?></p>
-                                    <p><strong>Location:</strong> <?= htmlspecialchars($shelter['location']); ?></p>
-                                    <p><strong>Working Day:</strong> <?= htmlspecialchars($shelter['workingDay']); ?></p>
-                                    <p><strong>Working Hours:</strong> <?= htmlspecialchars($shelter['workingHours']); ?></p>
-                                </div>
-                            </div>
-                            <div class="col-7 right-panel" style="margin-left: 10px; height: fit-content;">
-                                <div class="tab-content">
-                                    <div class="tab-pane fade show active" id="description">
-                                        <img src="data:image/jpeg;base64,<?= htmlspecialchars($shelter['imageDesc']); ?>" >
-                                        <p><?= htmlspecialchars($shelter['description']); ?></p>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                        <div class="footer-buttons text-center">
-                            <a href="<?= htmlspecialchars($shelter['link']); ?>"><button class="btn btn-custom">Facebook</button></a>
-                           
-                        </div>
                     </div>
                 </div>
-            </div>
+            <?php endforeach; ?>
         </div>
-    </div>
-    <?php endforeach; ?>
-    </div>
     </div>
 
 
 
     <section class="footer">
         <footer class="bg-dark text-white py-4 mt-5">
-          <div class="container">
-            <div class="row">
-              <div class="contact-info col-md-6">
-                <h5 class="mb-3">Pet Haven</h5>
-                <p>1234 Pet Street, Pet City, 56789</p>
-                <p>Email: info@petshelter.com | Phone: 123-456-7890</p>
-              </div>
-              <div class="col-md-6 text-md-right">
-                <a href="#" class="text-white me-2"><i class="fab fa-facebook-f"></i></a>
-                <a href="#" class="text-white me-2"><i class="fab fa-twitter"></i></a>
-                <a href="#" class="text-white"><i class="fab fa-instagram"></i></a>
-              </div>
+            <div class="container">
+                <div class="row">
+                    <div class="contact-info col-md-6">
+                        <h5 class="mb-3">Pet Haven</h5>
+                        <p>1234 Pet Street, Pet City, 56789</p>
+                        <p>Email: info@petshelter.com | Phone: 123-456-7890</p>
+                    </div>
+                    <div class="col-md-6 text-md-right">
+                        <a href="#" class="text-white me-2"><i class="fab fa-facebook-f"></i></a>
+                        <a href="#" class="text-white me-2"><i class="fab fa-twitter"></i></a>
+                        <a href="#" class="text-white"><i class="fab fa-instagram"></i></a>
+                    </div>
+                </div>
+                <hr class="footer-navigation bg-white" />
+                <div class="row">
+                    <div class="col-md-6">
+                        <nav>
+                            <a href="#home" class="text-white me-2">HOME</a> |
+                            <a href="#about" class="text-white me-2">ADOPT</a> |
+                            <a href="#contact" class="text-white me-2">DONATION</a> |
+                            <a href="#contact" class="text-white me-2">SHELTER</a> |
+                            <a href="#contact" class="text-white me-2">HELP</a> |
+                            <a href="#contact" class="text-white">LOGIN</a>
+                        </nav>
+                    </div>
+                    <div class="col-md-6 text-md-right">
+                        <p>© 2024 Pet Haven. All rights reserved.</p>
+                    </div>
+                </div>
             </div>
-            <hr class="footer-navigation bg-white" />
-            <div class="row">
-              <div class="col-md-6">
-                <nav>
-                  <a href="#home" class="text-white me-2">HOME</a> |
-                  <a href="#about" class="text-white me-2">ADOPT</a> |
-                  <a href="#contact" class="text-white me-2">DONATION</a> |
-                  <a href="#contact" class="text-white me-2">SHELTER</a> |
-                  <a href="#contact" class="text-white me-2">HELP</a> |
-                  <a href="#contact" class="text-white">LOGIN</a>
-                </nav>
-              </div>
-              <div class="col-md-6 text-md-right">
-                <p>© 2024 Pet Haven. All rights reserved.</p>
-              </div>
-            </div>
-          </div>
         </footer>
-      </section>
+    </section>
 
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
     <script>
-       
+
     </script>
 </body>
 
