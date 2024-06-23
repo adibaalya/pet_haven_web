a1b787cf8d3b5d02bd85f8309d14854b494563f2
shane-kennedy-se <216883@student.upm.edu.my>
2024-06-23 21:05:10 +0800

    Create account_page.php

---
+++ php/account_page.php
@@ -0,0 +1,35 @@
+</div>
+          <div class="col-md-6 text-md-right">
+            <a href="#" class="text-white me-2"><i class="fab fa-facebook-f"></i></a>
+            <a href="#" class="text-white me-2"><i class="fab fa-twitter"></i></a>
+            <a href="#" class="text-white"><i class="fab fa-instagram"></i></a>
+          </div>
+        </div>
+        <hr class="footer-navigation bg-white">
+        <div class="row">
+          <div class="col-md-6">
+            <nav>
+              <a href="#home" class="text-white me-2">HOME</a> |
+              <a href="#about" class="text-white me-2">ADOPT</a> |
+              <a href="#contact" class="text-white me-2">DONATION</a> |
+              <a href="#contact" class="text-white me-2">SHELTER</a> |
+              <a href="#contact" class="text-white me-2">HELP</a> |
+              <a href="#contact" class="text-white">LOGIN</a>
+            </nav>
+          </div>
+          <div class="col-md-6 text-md-right">
+            <p>© 2024 Pet Haven. All rights reserved.</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  </section>
+
+  <!-- Include necessary JavaScript libraries -->
+  <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  <script src="../javascript/account.js"></script>
+  <script src="../javascript/adopt_js.js"></script>
+</body>
+</html>
