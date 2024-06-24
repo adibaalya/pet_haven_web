3fdad5fceca5bfa7e751d97d8652e378b1b02f60
adibaalya <164612947+adibaalya@users.noreply.github.com>
2024-06-24 23:53:12 +0800

    .

--- php/adopt_page.php
+++ php/adopt_page.php
@@ -13,13 +13,25 @@
   <script src="https://cdnjs.cloudflare.com/ajax/libs/odometer.js/0.4.7/odometer.min.js"></script>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" />
   <title>Adopt</title>
+  <style>
+    .navbar-toggler {
+    z-index: 2;
+    border-color: black;
+}
+
+/* Change the color of the navbar toggler icon */
+.navbar-toggler .navbar-toggler-icon {
+    background-image: url("data:image/svg+xml;charset=utf8,%3Csvg viewBox='0 0 30 30' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath stroke='rgba(0, 0, 0, 1)' stroke-width='2' stroke-linecap='round' stroke-miterlimit='10' d='M4 7h22M4 15h22M4 23h22'/%3E%3C/svg%3E");
+}
+
+  </style>
 </head>
 
 <body>
   <div id="trailer">
   </div>
   <section class="nav-bar">
-    <nav class="navbar navbar-expand-lg navbar-dark ">
+    <nav class="navbar navbar-expand-lg navbar-dark  ">
     <a class="navbar-brand " href="#">
         <img src="../assets/images/logo.png" width="60" height="60" alt="Pet Haven Logo" />
       </a>
